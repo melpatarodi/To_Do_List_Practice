a604b2971f86174298d1d29691e96686cf80444d
Melody P. <melpatarodi@gmail.com>
2026-09-08 12:42:49 -0500

    Update list.h

--- list.h
+++ list.h
@@ -1,1 +1,17 @@
+#ifndef LIST.H
+#define LIST.H
+#include <string>
+#include <vector> 
 
+struct Job {
+string desc;
+bool finished;
+};
+
+void addTask(vector<Tasks>& tasks)
+void viewTask(const vector<Tasks>& tasks)
+void completeTask(vector<Tasks>& tasks)
+void deleteTask(vector<Tasks>& tasks)
+
+
+#endif
