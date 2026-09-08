01128206582f238d13bc0f7b30421d4c8cb145ff
Melody P. <melpatarodi@gmail.com>
2026-09-08 13:09:46 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,40 @@
+#include "list.h"
+#include <iostream>
+#include <vector>
 
+
+int main() {
+vector<Task> tasks;
+bool exit = false;
+int choice;
+
+while (!exit) {
+  cout << "_________TO DO LIST_________\n" << "1. Add task\n" << "2. View Tasks\n << 
+    "3. Mark completed task\n" << "4. Delete task\n" << "5. Exit\n" << "Enter a number: ";
+  cin >> choice;
+  switch (choice) {
+    case 1:  
+      addTask(tasks);
+      break;
+    case 2:
+      viewTasks(tasks);
+      break;
+    case 3:
+      completeTask(tasks);
+      break;
+    case 4:
+      deleteTask(tasks);
+      break;
+    case 5:
+      cout << "Goodbye!\n";
+      exit = true;
+      break;
+    default: 
+      cout << "Invalid. Try again.\n";
+      break;
+  }
+}
+
+
+return 0;
+}
