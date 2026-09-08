d66dd7eee376d2b7e7e9ffaaaa7874c710e29b94
Melody P. <melpatarodi@gmail.com>
2026-09-08 13:27:41 -0500

    Update list.h

--- list.h
+++ list.h
@@ -10,10 +10,10 @@ string desc;
 bool finished;
 };
 
-void addTask(vector<Tasks>& tasks)
-void viewTasks(const vector<Tasks>& tasks)
-void completeTask(vector<Tasks>& tasks)
-void deleteTask(vector<Tasks>& tasks)
+void addTask(vector<Tasks>& tasks);
+void viewTasks(const vector<Tasks>& tasks);
+void completeTask(vector<Tasks>& tasks);
+void deleteTask(vector<Tasks>& tasks);
 
 
 #endif
